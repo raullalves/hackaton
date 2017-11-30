0b47d07bfc234fc6f9e12351f8007adae2ee80e3
Raul Lima Alves <raullalves@gmail.com>
2017-11-30 12:58:25 -0200

    modified

--- mqttTeste.js
+++ mqttTeste.js
@@ -1,13 +1,13 @@
 var mqtt = require('mqtt')
 var request = require('ajax-request');
-var servidor  = mqtt.connect('mqtt://test.mosquitto.org')
+var servidor  = mqtt.connect('tcp://127.0.0.1:1883')
  
 servidor.on('connect', function () {
   servidor.subscribe('mqtt/hackaton/enviar/#')
 })
  
 servidor.on('message', function (topic, message) {
-
+  console.log(message)
   var messageArray = message.toString().split(";")
   var latitude = messageArray[0]
   var longitude = messageArray[1]
@@ -28,10 +28,10 @@ servidor.on('message', function (topic, message) {
     var jsonPrased = JSON.parse(body.toString())
     var temperatura = jsonPrased.main.temp - 273.15
     var pressao = jsonPrased.main.pressure
-    var humidade = jsonPrased.main.humidity
+    var umidade = jsonPrased.main.humidity
     console.log("temperatura = "+Math.round(temperatura*10)/10)
     console.log("pressao = "+pressao)
-    console.log("humidade = "+humidade)
+    console.log("umidade = "+umidade)
   });
   
   servidor.publish(topicReceber, 'nao irrigue')
--- mqttTesteCliente.js
+++ mqttTesteCliente.js
@@ -3,7 +3,7 @@ var client  = mqtt.connect('mqtt://test.mosquitto.org')
  
 client.on('connect', function () {
   client.publish('mqtt/hackaton/enviar/id5', '-23.5505199;-46.6333094')
-  client.subscribe('mqtt/hackaton/receber/#')
+  client.subscribe('mqtt/hackaton/receber/id5')
 })
  
 client.on('message', function (topic, message) {
